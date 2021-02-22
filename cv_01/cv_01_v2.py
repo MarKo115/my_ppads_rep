5b1dee29f51708be27cac206320528b54f33faf5
Marek <markotes23@gmail.com>
2021-02-22 21:34:15 +0100

    feat: Create 2rd variant of code from cvicenie 1 and update Description.txt file

---
+++ cv_01/cv_01_v2.py
@@ -0,0 +1,33 @@
+# .py file for 1st project
+from fei.ppds import Thread
+from fei.ppds import Mutex
+from collections import Counter
+
+
+class Shared():
+    def __init__(self, end):
+        self.counter = 0
+        self.end = end
+        self.elms = [0] * self.end
+        self.mutex = Mutex()
+
+
+def fnc_counter(shared):
+    while True:
+        if shared.counter >= shared.end:
+            return
+        shared.mutex.lock()
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
+
+
+for i in range(10):
+    sh = Shared(1_000_000)
+    t1 = Thread(fnc_counter, sh)
+    t2 = Thread(fnc_counter, sh)
+
+    t1.join()
+    t2.join()
+
+    print(Counter(sh.elms))
