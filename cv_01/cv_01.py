46673eebba3c469f3ca1c129c55ae326b4fb35dd
Marek <markotes23@gmail.com>
2021-02-22 10:11:14 +0100

    feat: Adding .gitignore file and create basic code for 1st exercise

--- cv_01/cv_01.py
+++ cv_01/cv_01.py
@@ -1,1 +1,30 @@
-"# .py file for 1st project" 
+# .py file for 1st project
+from fei.ppds import Thread
+from fei.ppds import Mutex
+from collections import Counter
+
+
+class Shared():
+    def __init__(self, end):
+        self.counter = 0
+        self.end = end
+        self.elms = [0] * self.end
+
+
+def fnc_counter(shared):
+    while True:
+        if shared.counter >= shared.end:
+            break
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+
+
+for i in range(10):
+    sh = Shared(1_000_000)
+    t1 = Thread(fnc_counter, sh)
+    t2 = Thread(fnc_counter, sh)
+
+    t1.join()
+    t2.join()
+
+    print(Counter(sh.elms))
