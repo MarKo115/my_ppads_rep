48a24c03405e9b515d32ee3bf5939c7ef5d17f5e
Marek <markotes23@gmail.com>
2021-03-07 20:10:33 +0100

    feat: Create implementation for ADT Lightswitch

--- cv_03/cv_03_lightswitch.py
+++ cv_03/cv_03_lightswitch.py
@@ -1,12 +1,24 @@
+from fei.ppds import Mutex
+
+
 class ADTLightswitch:
     def __init__(self):
-        pass
+        self.mutex = Mutex()
+        self.counter = 0
 
     def lock(self, sem):
-        pass
+        self.mutex.lock()
+        self.counter += 1
+        if self.counter == 1:
+            sem.wait()
+        self.mutex.unlock()
 
     def unlock(self, sem):
-        pass
+        self.mutex.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            sem.signal()
+        self.mutex.unlock()
 
 
-print()
+
