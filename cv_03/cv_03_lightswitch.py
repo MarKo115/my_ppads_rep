9459a6b1710376dd698087b9f4cfdaaeda9ec0e2
Marek <markotes23@gmail.com>
2021-03-09 08:49:14 +0100

    feat: Update code for model Readers-Writers with starvation

--- cv_03/cv_03_lightswitch.py
+++ cv_03/cv_03_lightswitch.py
@@ -10,6 +10,7 @@ class ADTLightswitch:
     def __init__(self):
         self.mutex = Mutex()
         self.counter = 0
+        self.turnstile = Semaphore(1)
 
     def lock(self, sem):
         self.mutex.lock()
@@ -27,43 +28,51 @@ class ADTLightswitch:
 
 
 def fnc_read(thread_id):
-    sleep(randint(1, 10) / 10)
+    sleep(randint(1, 10) / 100)
     print(f"{thread_id} reading")
 
 
 def fnc_write(thread_id):
-    sleep(randint(1, 10) / 10)
+    sleep(randint(1, 10) / 100)
     print(f"{thread_id} writing")
 
 
 def readers_light_switch(light_switch, room_e, reader_id):
-    light_switch.lock(room_e)
-    fnc_read(reader_id)
-    light_switch.unlock(room_e)
+    while True:
+        light_switch.turnstile.wait()
+        light_switch.turnstile.signal()
+        light_switch.lock(room_e)
+        fnc_read(reader_id)
+        light_switch.unlock(room_e)
 
 
-def writers_light_switch(room_e, writer_id):
-    room_e.wait()
-    fnc_write(writer_id)
-    room_e.signal()
+def writers_light_switch(light_switch, room_e, writer_id):
+    while True:
+        light_switch.turnstile.wait()
+        room_e.wait()
+        fnc_write(writer_id)
+        room_e.signal()
+        light_switch.turnstile.signal()
+        sleep(0)
 
 
 num_r = 10
 arr_readers = [0] * num_r
-num_w = 10
+num_w = 7
 arr_writers = [0] * num_w
 adt_lightswitch = ADTLightswitch()
 empty_room = Semaphore(1)
 
-for j in range(num_w):
-    arr_writers[j] = Thread(writers_light_switch, empty_room, j)
-
 for i in range(num_r):
     arr_readers[i] = Thread(readers_light_switch, adt_lightswitch, empty_room, i+50)
+
+for j in range(num_w):
+    arr_writers[j] = Thread(writers_light_switch, adt_lightswitch, empty_room, j)
 
 for t in arr_writers:
     t.join()
 
 for t in arr_readers:
     t.join()
+
 
