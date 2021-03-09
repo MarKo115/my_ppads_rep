eb082b6cadf9170783d25a8495c64c033955b6cd
Marek <markotes23@gmail.com>
2021-03-09 08:37:08 +0100

    feat: Add code for model Readers-Writers without starvation

--- cv_03/cv_03_lightswitch.py
+++ cv_03/cv_03_lightswitch.py
@@ -1,4 +1,9 @@
 from fei.ppds import Mutex
+from fei.ppds import Semaphore
+from fei.ppds import Thread
+from fei.ppds import print
+from time import sleep
+from random import randint
 
 
 class ADTLightswitch:
@@ -21,4 +26,44 @@ class ADTLightswitch:
         self.mutex.unlock()
 
 
+def fnc_read(thread_id):
+    sleep(randint(1, 10) / 10)
+    print(f"{thread_id} reading")
+
+
+def fnc_write(thread_id):
+    sleep(randint(1, 10) / 10)
+    print(f"{thread_id} writing")
+
+
+def readers_light_switch(light_switch, room_e, reader_id):
+    light_switch.lock(room_e)
+    fnc_read(reader_id)
+    light_switch.unlock(room_e)
+
+
+def writers_light_switch(room_e, writer_id):
+    room_e.wait()
+    fnc_write(writer_id)
+    room_e.signal()
+
+
+num_r = 10
+arr_readers = [0] * num_r
+num_w = 10
+arr_writers = [0] * num_w
+adt_lightswitch = ADTLightswitch()
+empty_room = Semaphore(1)
+
+for j in range(num_w):
+    arr_writers[j] = Thread(writers_light_switch, empty_room, j)
+
+for i in range(num_r):
+    arr_readers[i] = Thread(readers_light_switch, adt_lightswitch, empty_room, i+50)
+
+for t in arr_writers:
+    t.join()
+
+for t in arr_readers:
+    t.join()
 
