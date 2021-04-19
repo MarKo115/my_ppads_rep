62dff7d250fe6e13d3802e74a6f7bd610341e294
Marek <markotes23@gmail.com>
2021-04-19 20:59:40 +0200

    feat: Rewrite code from internet to asynchronous code

--- cv_08/asynchronous_exercise_1.py
+++ cv_08/asynchronous_exercise_1.py
@@ -1,31 +1,44 @@
 # Written by Anton Caceres
 # https://github.com/MA3STR0/PythonAsyncWorkshop
-
-
-from urllib.request import urlopen
 import time
-
-URLS = [
-    'http://dsl.sk',
-    'http://stuba.sk',
-    'http://shmu.sk',
-    'http://root.cz',
-]
+import asyncio
+import aiohttp
 
 
-def request_greetings():
+async def request_greetings(name, work_queue):
     responses = []
-    for url in URLS:
-        resp = urlopen(url)
-        responses.append(resp.read().decode('latin-1'))
-    texts = '\n'.join(responses)
-    return texts
+    async with aiohttp.ClientSession() as session:
+        while not work_queue.empty():
+            url = await work_queue.get()
+            print(f"Task {name} getting URL: {url}")
+            async with session.get(url) as response:
+                res = await response.text()
+            responses.append(res)
+        texts = '\n'.join(responses)
+        return texts
+
+
+async def main():
+    # Create the queue of work
+    work_queue = asyncio.Queue()
+
+    # Put some work in the queue
+    for url in [
+        'http://dsl.sk',
+        'http://stuba.sk',
+        'http://shmu.sk',
+        'http://root.cz',
+    ]:
+        await work_queue.put(url)
+
+    # Run tasks
+    t1 = time.time()
+    greetings = await asyncio.gather(request_greetings("One", work_queue), request_greetings("Two", work_queue))
+    print(time.time() - t1, "seconds passed")
+    print(greetings)
 
 
 if __name__ == "__main__":
-    t1 = time.time()
-    greetings = request_greetings()
-    print(time.time() - t1, "seconds passed")
-    print(greetings)
+    asyncio.get_event_loop().run_until_complete(main())
 
 
